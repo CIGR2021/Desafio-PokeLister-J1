7779d5991e3d0e6f4e1698cca19fec02a6ed2aad
CIGR2021 <appencigr@gmail.com>
2022-02-16 15:59:23 -0300

    Mudei um pouco o CSS e inclui a rotapara o CardView

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import {
   Switch,
   Route,
+  // Redirect
 } from 'react-router-dom';
 import './App.css'
 import CardList from './components/CardList';
+import CardView from './view/CardView';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ CardList } />
-      {/* <Route exact path="/pokemon/:name" component={ View } /> */}
+      <Route exact path="/pokemon/:name" component={ CardView } />
+      {/* <Route path="/" exact>
+        <Redirect to="/pokemons/list" />
+      </Route> */}
     </Switch>
   );
 }
