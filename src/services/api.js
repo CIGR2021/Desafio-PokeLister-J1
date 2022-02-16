bed9d88b4829e06011f97c24df449a6d7ebed921
CIGR2021 <appencigr@gmail.com>
2022-02-16 16:02:26 -0300

    Ajustei as chamadas para a api

--- src/services/api.js
+++ src/services/api.js
@@ -2,20 +2,20 @@ import axios from 'axios';
 
 const API_URL = 'https://pokeapi.co/api/v2/pokemon';
 
-// https://pokeapi.co/api/v2/pokemon-form/
 export const getAll = async() => {
   try {
-    const dataUrls = await axios.get(`${API_URL}-form?offset=0&limit=150`);
+    const dataUrls = await axios.get(`${API_URL}?offset=0&limit=150`);
     return dataUrls.data.results
   } catch (err) {
     console.log(err)
   }
 };
 
-// export const getByName = async(name) => {
-//   try {
-//     return await axios.get(`${API_URL}{name}`);
-//   } catch (err) {
-//     console.log(err)
-//   }
-// };
+export const getByName = async(name) => {
+  try {
+    const result = await axios.get(`${API_URL}/${name}`);
+    return result.data
+  } catch (err) {
+    console.log(err)
+  }
+};
