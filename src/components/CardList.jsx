24e1de9a04ea4e4f6007b0e7e9bb1a1bef9d6e0f
CIGR2021 <appencigr@gmail.com>
2022-02-17 18:26:35 -0300

    Implementei a paginação e apliquei material ui

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,31 +1,45 @@
+import Pagination from "./Pagination";
 import React, { useEffect, useState } from "react";
 import { getAll } from "../services/api";
-import Card from './Card';
-import Pagination from "./Pagination";
+import CardComponent from './Card';
+import { Container } from "@mui/material";
 
 function CardList() {
   const [pokemons, setPokemons] = useState([])
-  const [offset, setOffset] = useState(0)
-  const OFFSET = offset;
-  const LIMIT = 24;
+  const [currentPage, setCurrentPage] = useState(0)
+  const [itensPerPage, setItensPerPage] = useState(24)
+  const pages = Math.ceil(pokemons.length / itensPerPage)
+  const startIndex = currentPage * itensPerPage
+  const endIndex = startIndex + itensPerPage
+  const currentPokemons = pokemons.slice(startIndex, endIndex)
+  const OFFSET = currentPage;
+  const LIMIT_TOTAL_POKEMONS = 150;
+
   const getAllPokemons = async() => {
-    const data = await getAll(OFFSET, LIMIT)
+    const data = await getAll(OFFSET, LIMIT_TOTAL_POKEMONS)
     setPokemons(data)
   }
 
   useEffect(() => {
     getAllPokemons();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
-    <main>
+    <Container maxWidth="lx">
       <section className="card-list">
-        { pokemons.map((pokemon) => (
-          <Card key={ pokemon.name } pokemon={ pokemon } />
+        { currentPokemons.map((pokemon) => (
+          <CardComponent key={ pokemon.name } pokemon={ pokemon } />
         ))}
       </section>
-      <Pagination />
-    </main>
+      <Pagination
+        pages={pages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        itensPerPage={itensPerPage}
+        setItensPerPage={setItensPerPage}
+      />
+    </Container>
   )
 }
 
--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,43 +1,55 @@
-import * as React from 'react';
-import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
+import { Button, ButtonGroup, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import React from "react";
 
-export default function BasicPagination() {
+function Pagination({
+  pages,
+  currentPage,
+  setCurrentPage,
+  itensPerPage,
+  setItensPerPage
+}) {
   return (
-    <Stack spacing={0}>
-      <Pagination count={7} color="primary" />
-    </Stack>
-  );
+    <ul className="pagination">
+        <FormControl className="pagination">
+          <InputLabel id="Quantidade por página">Quantidade por página</InputLabel>
+          <Select
+            labelId="Quantidade por página"
+            id="Quantidade por página"
+            defaultValue={itensPerPage}
+            label="Quantidade por página"
+            onChange={({ target }) => setItensPerPage(Number(target.value))}
+          >
+            <MenuItem value={6}>6</MenuItem>
+            <MenuItem value={12}>12</MenuItem>
+            <MenuItem value={18}>18</MenuItem>
+            <MenuItem value={24}>24</MenuItem>
+          </Select>
+        </FormControl>
+        <InputLabel sx={{ marginLeft: 2 }}>
+          Pages
+          {Array.from(Array(pages), (item, index) => {
+            return (
+              <FormControl className="pagination">
+                <li>
+                  <ButtonGroup
+                    sx={{ marginLeft: 2 }}
+                    disableElevation
+                    variant={index === currentPage ? 'contained' : 'text'}
+                  >
+                    <Button
+                      value={index}
+                      onClick={({ target }) => setCurrentPage(Number(target.value))}
+                    >
+                      {index + 1}
+                    </Button>
+                  </ButtonGroup>
+                </li>
+              </FormControl>
+            )
+          })}
+        </InputLabel>
+      </ul>
+  )
 }
 
-// import React from "react";
-
-// const MAX_ITEMS = 5;
-// const MAX_LEFT = (MAX_ITEMS - 1) / 2;
-
-// const Pagination = ({ limit, total, offset, setOffset }) => {
-//   const currentPage = offset ? (offset / limit) + 1 : 1;
-//   const totalPages = Math.ceil(total / limit);
-//   const firstPage = Math.max(currentPage - MAX_LEFT, 1)
-//   return (
-//     <ul className="pagination">
-//       {Array.from({ length: Math.min(MAX_ITEMS, totalPages) })
-//         .map((_, indice) => indice + firstPage)
-//         .map((page) => (
-//           <li key={page}>
-//             <button
-//               onClick={() => setOffset((page - 1) * limit)}
-//               className={
-//                 page === currentPage
-//                 ? "pagination__item--active"
-//                 : null
-//               }
-//             >
-//               {page}
-//             </button>
-//           </li>))}
-//     </ul>
-//   )
-// }
-
-// export default Pagination;
+export default Pagination;
